2245d7c1f5ec2a3416642be253f985eedb8a3fcc
Asvin Goel <github@telematique.eu>
2025-07-19 19:36:27 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -11,6 +11,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-  repo = "https://github.com/$(ENV["GITHUB_REPOSITORY"]).git",
+  repo = "github.com/$(ENV["GITHUB_REPOSITORY"]).git",
   devbranch = "main"
 )
